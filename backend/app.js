fe4ace5fa89e31295a7f84a4edd51124a5bdbfa1
Prashant Sharma <prashantsharma6002002@gmail.com>
2024-08-19 20:37:12 +0530

    debugging

--- backend/app.js
+++ backend/app.js
@@ -4,31 +4,32 @@ import user from './routes/user.routes.js';
 import auSign from './routes/auth.routes.js'; 
 import cookieParser from "cookie-parser";
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Initialize Express App
 const app = express();
+
+// Determine __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(cookieParser());
 
+// Serve static files
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
-app.use(cookieParser());
-// app.use(cors({
-//     origin: 'http://localhost:5173', // Hardcoded origin
-//     credentials: true
-// }));
 
-
-//
+// Define Routes
 app.use("/api/v1/user", user);
-
 app.use("/api/v1/auth", auSign);
 
 export { app };
