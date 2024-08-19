ea6e7f2027e7e9841b7c25975f36a032762ff1a7
Prashant Sharma <prashantsharma6002002@gmail.com>
2024-08-20 00:47:03 +0530

    debugg

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/v1': 'https://auth-boilerplate-1.onrender.com',
+      'api/v1': 'https://auth-boilerplate-1.onrender.com',
     },
   },
   
