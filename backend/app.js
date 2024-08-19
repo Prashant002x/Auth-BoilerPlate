853015ca2f6db97851837ad57da17a179f29b2e1
Prashant Sharma <prashantsharma6002002@gmail.com>
2024-08-19 21:23:01 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://auth-boiler-plate-qx51.vercel.app/",
     credentials: true
 }));
 
